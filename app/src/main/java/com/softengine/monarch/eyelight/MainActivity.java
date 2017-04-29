660857f060a46fbb1207f34294ac8bb44f6f95a5
mehmet <mhmtonar1@gmail.com>
2017-04-29 22:31:21 +0300

    a
    aa

---
+++ app/src/main/java/com/softengine/monarch/eyelight/MainActivity.java
@@ -0,0 +1,48 @@
+package com.softengine.monarch.eyelight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnShowLocation;
+
+    // GPSManager class
+    GPSManager gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnShowLocation = (Button) findViewById(R.id.Button);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSManager(MainActivity.this);
+
+                // check if GPS enabled     
+                if(gps.isGPSEnabled){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+    
+}
